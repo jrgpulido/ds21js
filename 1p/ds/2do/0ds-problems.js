25e35fbca7fc4e02ed6eed89b906053bddc8d50f
jrgpulido <jrgpulido@gmail.com>
2021-03-12 13:14:27 -0600

    first commit on my repo

---
+++ 1p/ds/2do/0ds-problems.js
@@ -0,0 +1,27 @@
+//
+// create one file by program
+// implement a method, function, non-function solution
+// solve each as requested
+// may use code from previous exercises
+//
+// DS in JS
+
+//1. object
+//   attach(n)
+// -> n random integer
+// <- random name added
+
+//2. set
+//   attach(n)//wrap the method 'add'
+// -> n random integer
+// <- 
+
+//3. map
+//   attach(n)//wrap the method 'set'
+// -> n random integer
+// <- 
+
+//4. array
+//   attach(n)
+// -> n random integer
+// <- random name added
